ff1b85f4a76643581ab9ac2e1e9ddfceac5055d2
novikovaai <137191975+novikovaai@users.noreply.github.com>
2024-01-10 21:44:45 +0300

    Пройден раздел Расчеты и Math до домашнего задания

--- js-adv/scripts/app.js
+++ js-adv/scripts/app.js
@@ -21,3 +21,54 @@ for (let [key, value] of weatherMap) {
 }
 
 console.log(weatherMap)
+
+//Сделать функцию, которая принимает min и max и возвращает случайное число между ними
+
+const min = 1;
+const max = 20;
+
+function randomNumber(min, max) {
+	const scale = Math.random();
+	return Math.floor(scale * (max - min + 1) + min)
+
+}
+
+randomNumber(min, max)
+
+// Сделать конвертер валют
+
+function currencyConverter(amount, initialCurrency, targetCurrency) {
+	const curRub = {
+		style: 'currency',
+		currency: 'RUB'
+	}
+	const curUsd = {
+		style: 'currency',
+		currency: 'USD'
+	}
+	const curEur = {
+		style: 'currency',
+		currency: 'EUR'
+	}
+	switch (initialCurrency) {
+		case 'RUB': switch (targetCurrency) {
+			case 'USD': return new Intl.NumberFormat('ru-RU', curUsd).format(amount / 89.7619);
+			case 'EUR': return new Intl.NumberFormat('ru-RU', curEur).format(amount / 97.9126);
+			default: return null;
+		}
+		case 'USD': switch (targetCurrency) {
+			case 'RUB': return new Intl.NumberFormat('en-US', curRub).format(amount * 89.7619);
+			case 'EUR': return new Intl.NumberFormat('en-US', curEur).format(amount * 0.9168);
+			default: return null;
+		}
+		case 'EUR': switch (targetCurrency) {
+			case 'RUB': return new Intl.NumberFormat('en-EN', curRub).format(amount * 97.9126);
+			case 'USD': return new Intl.NumberFormat('en-EN', curUsd).format(amount * 1.0908);
+			default: return null;
+		}
+		default:
+			return null;
+	}
+}
+
+console.log(currencyConverter(1000, 'RUB', 'EUR'));
