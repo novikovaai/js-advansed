6e6495361bfc6acf00ca86c624325a22e3a8cd8a
novikovaai <137191975+novikovaai@users.noreply.github.com>
2024-01-17 18:29:56 +0300

    Добавлен error

--- 9-pokemon.js
+++ 9-pokemon.js
@@ -1,14 +1,16 @@
 'use strict';
 
+
+
 const request = new XMLHttpRequest();
-request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
-request.send();
+
+
+
+request.addEventListener("error", event => console.log('Произошла ошибка!'))
 request.addEventListener("load", function () {
 	const abUrl = JSON.parse(this.responseText).abilities[0].ability.url
-	
+
 	const request = new XMLHttpRequest();
-	request.open('GET', abUrl);
-	request.send();
 	request.addEventListener("load", function () {
 		const data = JSON.parse(this.responseText).effect_entries
 		for (const entry of data) {
@@ -17,4 +19,10 @@ request.addEventListener("load", function () {
 			}
 		}
 	})
+	request.open('GET', abUrl);
+	request.send();
+
 })
+
+request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
+request.send(); 
