68a866e6b35b9cb811799bd587080c29582167e9
novikovaai <137191975+novikovaai@users.noreply.github.com>
2024-01-18 15:23:28 +0300

    Пройдены Promises до дз

--- js-adv/scripts/app.js
+++ js-adv/scripts/app.js
@@ -1,4 +1,31 @@
 'use strict';
 
-fetch('https://dummyjson.com/products/1').then(response => response.json()).then(data => data)
+function getData(url, errMsg) {
+	return fetch(url)
+		.then(
+			response => {
+				if (!response.ok) {
+					throw new Error(`${errMsg} ${response.status}`)
+				}
+				return response.json()
+			}
+		)
+}
+
+getData('https://dummyjson.com/products/', 'Страница не найдена')	
+	.then(({ products }) => {
+		console.log(products);
+		return getData('https://dummyjson.com/products/' + products[0].id, 'Товар не найден')
+	})
+	.then(response => response.json())
+	.then(data => {
+		console.log(data)
+	})
+	.catch(error => {
+		const el = document.querySelector('.secondTest');
+		el.innerHTML = error.message
+	})
+
+
+
 
